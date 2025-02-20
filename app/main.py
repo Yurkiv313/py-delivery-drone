4b7113ba86eac94b5ece57f01b9c73db53d4594a
Andrii <yurkivandriy02@gmail.com>
2025-02-20 11:16:17 +0200

    fix code style

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ class Cargo:
 
 class BaseRobot:
     def __init__(
-        self, name: str, weight: int, coords: list | None = None
+        self,
+        name: str,
+        weight: int,
+        coords: list | None = None
     ) -> None:
         self.name = name
         self.weight = weight
